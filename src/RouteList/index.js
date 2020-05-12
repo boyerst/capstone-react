282faa524033748f9a38ffbba455905646385858
Steve Boyer <stephen.a.boyer@gmail.com>
2020-05-12 18:49:17 -0500

    App renders a Route list with semantic UI

--- src/RouteList/index.js
+++ src/RouteList/index.js
@@ -1,12 +1,46 @@
 import React from 'react'
-import { Button, Image, Item } from 'semantic-ui-react'
+import { Button, Image, Item, Rating, Grid, Segment } from 'semantic-ui-react'
+import StackGrid from "react-stack-grid";
 
 export default function RouteList(props) {
 
   console.log("Here are the props in RouteList:")
   console.log(props)
 
+  const routes = props.routes.map(route => {
+    return (
+     
+     <StackGrid 
+      key={route.id}
+      columnWidth={850}
+      >
+        <Item.Group relaxed>
+          <Item>
+            <Item.Image size='small' src='https://react.semantic-ui.com/images/wireframe/image.png' />
+            <Item.Content verticalAlign='middle'>
+              <Item.Header>{route.location}</Item.Header> 
+                  <br/>
+                  <br/>
+              <Rating icon="star" maxRating="5" rating={route.skill_level} disabled/>
+              <Item.Description align="left">{route.comments}</Item.Description>
+              <Item.Extra align="left">{route.length}</Item.Extra>
+              <Item.Extra>
+                <Button floated='right'>See More</Button>
+              </Item.Extra>
+            </Item.Content>
+          </Item>
+    </Item.Group>
+    </StackGrid>
+
+
+   
+    )
+  }) 
   return (
-    <h2>Route List</h2>
+    <Item.Group>
+      {routes}
+    </Item.Group>
   )
-} 
+}
+
+
