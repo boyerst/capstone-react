709c40f4b9f6ec74107d1e6877835b63a2975b47
Steve Boyer <stephen.a.boyer@gmail.com>
2020-05-11 20:57:50 -0500

    finished Login/Regsiter condition logic

--- src/LoginRegistrationForm/index.js
+++ src/LoginRegistrationForm/index.js
@@ -14,52 +14,62 @@ export default class LoginRegisterForm extends Component {
     }
   }
 
+  changeForm = () => {
+    if(this.state.action==="Login"){
+      this.setState({action: "Register"})
+    } else {
+      this.setState({action: "Login"})
+    }
+  }
+
   render() {
     return (
       <Grid>
-      <Grid.Column>
-      <Segment>
-        <Form>
-          {this.state.action==="Register"
-          &&
-            <Form.Input 
-              type="text"
-              name="username"
-              placeholder="Enter a username"
-              value={this.state.username}
-            />
-          }
-       
-          <Form.Input 
-            type="text"
-            name="username"
-            placeholder="Enter a username"
-            value={this.state.username}
-          />
-      
-          <Form.Input 
-            type="text"
-            name="username"
-            placeholder="Enter a username"
-            value={this.state.username}
-          />
-          <Button type="Submit">Log In</Button>
-        </Form>
-        </Segment>
-        <Message>
-        {
-          this.state.action==="Login"
-          ?
-          <Message>
-          Need an account? <span className="link" onClick={this.changeForm}>Register</span>
-          </Message> 
-          :
-          <Message>
-          Already registered? <span className="link" onClick={this.changeForm}>Log In</span>
+        <Grid.Column>
+          <Segment>
+            <Form>
+              {this.state.action==="Register"
+              &&
+                <Form.Input 
+                  type="text"
+                  name="username"
+                  placeholder="Enter a username"
+                  value={this.state.username}
+                />
+              }
+           
+              <Form.Input 
+                type="text"
+                name="username"
+                placeholder="Enter a username"
+                value={this.state.username}
+              />
+          
+              <Form.Input 
+                type="text"
+                name="username"
+                placeholder="Enter a username"
+                value={this.state.username}
+              />
+              <Button type= "Submit" color="grey" fluid size="large">
+                {this.state.action==="Login" ? "Log In": "Register"}
+              </Button>
+            </Form>
+            </Segment>
+            <Message>
+            {
+              this.state.action==="Login"
+              ?
+              <Message>
+              Need an account? <span className="link" onClick={this.changeForm}>Register</span>
+              </Message> 
+              :
+              <Message>
+              Already registered? <span className="link" onClick={this.changeForm}>Log In</span>
+              </Message>
+            }
           </Message>
-        }
-      </Message>
-      </Grid.Column>
+        </Grid.Column>
       </Grid>      
     )
   }
