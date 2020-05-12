77f3015fc8e098afe19de96ea116572b1d72651a
Steve Boyer <stephen.a.boyer@gmail.com>
2020-05-11 21:10:47 -0500

    user can register

--- src/App.js
+++ src/App.js
@@ -17,9 +17,34 @@ export default class App extends Component {
   }
   
 
-  register = (registerInfo) => {
-    console.log("register() in App.js called with the following info", registerInfo);    
+   register = async (registrationInfo) => {
+    console.log("Here is the lifted registration data:", registrationInfo);
+    const url = process.env.REACT_APP_API_URL + '/api/v1/users/register'
+    try {
+      const registerResponse = await fetch(url, {
+        credentials: 'include', 
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(registrationInfo),
+      })
+      console.log("registerResponse", registerResponse)
+      const registerJson = await registerResponse.json()
+      console.log("registerJson", registerJson)
+      if(registerResponse.status === 201) {
+        this.setState({
+          loggedIn: true, 
+          loggedInUserEmail: registerJson.data.email
+        })
+      }
+    } catch(error) {
+      console.log("Error in registration route")
+      console.log(error)
+    } 
   }
+
+
 
   login = (loginInfo) => {
     console.log("login() in App.js called with the following info", loginInfo);    
