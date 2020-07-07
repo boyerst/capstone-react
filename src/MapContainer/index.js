dec700360fedeb0a80f3e18ac8ea204a41138c7c
Steve Boyer <stephen.a.boyer@gmail.com>
2020-07-07 17:59:30 -0500

    heroku debug

--- src/MapContainer/index.js
+++ src/MapContainer/index.js
@@ -45,26 +45,6 @@ export class MapContainer extends Component {
     }
   };
 
-  deleteMarker = async (idOfMarkerToDelete) => {
-    const url = process.env.REACT_APP_API_URL + "/api/v1/markers/" + idOfMarkerToDelete
-    try {
-    
-      const deleteMarkerResponse = await fetch(url, {
-        credentials: 'include',
-        method: 'DELETE'
-      })
-      console.log("deleteMarkerResponse", deleteMarkerResponse)
-      const deleteMarkerJson = await deleteMarkerResponse.json()
-      console.log("deleteMarkerJson", deleteMarkerJson)
-      this.setState({
-        markers: this.state.markers.filter(marker => marker.id != idOfMarkerToDelete)
-      })
-
-    } catch (err) {
-      console.log("error deleting the marker")
-      console.log(err)
-    }
-  }
   
   getMarkers = async () => {
     try {
@@ -125,6 +105,26 @@ export class MapContainer extends Component {
     })
   }
 
+  deleteMarker = async (idOfMarkerToDelete) => {
+    const url = process.env.REACT_APP_API_URL + "/api/v1/markers/" + idOfMarkerToDelete
+    try {
+    
+      const deleteMarkerResponse = await fetch(url, {
+        credentials: 'include',
+        method: 'DELETE'
+      })
+      console.log("deleteMarkerResponse", deleteMarkerResponse)
+      const deleteMarkerJson = await deleteMarkerResponse.json()
+      console.log("deleteMarkerJson", deleteMarkerJson)
+      this.setState({
+        markers: this.state.markers.filter(marker => marker.id != idOfMarkerToDelete)
+      })
+
+    } catch (err) {
+      console.log("error deleting the marker")
+      console.log(err)
+    }
+  }
 
 
   updateMarker = async (updatedMarkerInfo) => {
