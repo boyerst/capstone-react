5b5614a49e70de65d05899479eaf582da32dff41
Steve Boyer <stephen.a.boyer@gmail.com>
2021-05-27 17:40:34 -0500

    targeting .stackgrid css

--- src/RouteList/index.js
+++ src/RouteList/index.js
@@ -19,6 +19,7 @@ export default function RouteList(props) {
         backgroundColor: "#FFD60B",
         marginLeft: 150,
         marginRight: 100,
+        
 
       }}
       className="routeList"
