389e052111fc6056b3be081f59312971ff64a744
Steve Boyer <stephen.a.boyer@gmail.com>
2021-06-16 14:24:54 -0500

    removed semi colons from app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 import RouteContainer from './RouteContainer'
 import LoginRegistrationForm from './LoginRegistrationForm'
 import MenuContainer from './MenuContainer'
