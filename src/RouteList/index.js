83a36946e83c63a4ad7f2050b94ecd5ac9f54a81
Steve Boyer <stephen.a.boyer@gmail.com>
2021-06-16 14:29:58 -0500

    reverting routeList style changes

--- src/RouteList/index.js
+++ src/RouteList/index.js
@@ -15,6 +15,11 @@ export default function RouteList(props) {
   const routes = props.routes.map(route => {
     return (
      <StackGrid 
+        style={{
+          backgroundColor: "#FFD60B",
+          marginLeft: 150,
+          marginRight: 100,
+        }}
       className="routeList"
       key={route.id}
       columnWidth={1050}
