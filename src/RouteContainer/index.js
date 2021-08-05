461ec594460d0c0a0d15eb77cde96ec9fb623db0
Steve Boyer <stephen.a.boyer@gmail.com>
2021-08-05 14:36:09 -0500

    console.error(jqzr...

--- src/RouteContainer/index.js
+++ src/RouteContainer/index.js
@@ -188,7 +188,7 @@ export default class RouteContainer extends Component {
       })
     } catch(err) {
       console.error("Error getting route data.", err)
-      console.warn(jqxhr.responseText)
+      console.error(jqxhr.responseText)
     }
   }
 
